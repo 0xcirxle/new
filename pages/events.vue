5344b36f01e85a943f1628979d0636149f4904b5
Aniruddh Yadav <aniruddhyadav@Aniruddhs-MacBook-Air.local>
2024-07-02 11:23:27 +0530

    Empty commit to clear repository contents

--- pages/events.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="mt-12 px-4">
-       <h2 class="text-2xl font-semibold">Events</h2>
-    </div>
-</template>
--- pages/resources.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="mt-12 px-4">
-       <h2 class="text-2xl font-semibold">Resources</h2>
-    </div>
-</template>
