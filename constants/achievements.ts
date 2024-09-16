7007f31d6d452212514026a3084bdf473bd30757
0xcirxle <aniruddhiitree@gmail.com>
2024-09-16 13:31:46 +0530

    new achievements page

--- constants/achievements.ts
+++ constants/achievements.ts
@@ -2,57 +2,144 @@ export const Achievements = [
     {
         title: "Summer of Nix 2024",
         year: "2024",
+        image: "/achievement_images/nix.jpeg",
+        winners: [
+            {
+                description: "Shourya Goel selected for Summer of Nix 2024",
+            },
+        ]
     },
     {
         title: "Google Summer of Code 2024",
         year: "2024",
+        image: "/achievement_images/gsoc.png",
+        winners: [
+            {
+                description: "Shourya Goel selected for GSOC 2024",
+            },
+        ]
     },
     {
         title: "Summer of Bitcoin 2024",
         year: "2024",
+        image: "/achievement_images/sob.png",
+        winners: [
+            {
+                description: "Prabhat Verma selected for SOB at Bitcoin Core",
+            },
+            {
+                description: "Shashank Trivedi selected for SOB",
+            },
+        ] 
     },
     {
         title: "Winners at Scaling Ethereum",
         year: "2024",
+        image: "/achievement_images/scalingeth.png",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Winners at ETHMumbai Hackathon",
         year: "2024",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Winners at Circuit Breakers",
         year: "2024",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Winners at LFG GHO Hackathon",
         year: "2024",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Silver Medal at Inter IIT Tech 12.0 : Aptos Labs PS",
         year: "2023",
+        image: "/achievement_images/interiit12.png",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Winners at ETHIndia Hackathon",
         year: "2023",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Winners at ink! Polkadot Hackathon",
         year: "2023",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Finalists at Unfold",
         year: "2023",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Best Filecoin Project at ETHOnline",
         year: "2023",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "6th Position at Huffathon",
         year: "2023",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     },
     {
         title: "Best Mobile App & Best use of Subgraph at ETHGlobal Paris",
         year: "2023",
+        image: "/achievement_images/ethglobal.jpeg",
+        winners: [
+            {
+                description: "",
+            },
+        ]
     }
 ]
--- pages/achievements.vue
+++ pages/achievements.vue
@@ -3,19 +3,26 @@
     <h2 class="text-2xl font-semibold">Achievements</h2>
   </div>
   <div class="mt-4" v-for="(achievement, i) in Achievements" :key="i">
-    <div class="flex flex-row items-start justify-start w-full gap-4">
-      <div
-        class="flex flex-col items-center justify-center w-5 h-full gap-3 py-2"
-      >
-        <div class="w-[8px] h-[8px] rounded-full bg-gray-300" />
-        <div class="h-full min-h-[30px] border-r border-gray-300" />
-        <div v-if="i === Achievements.length - 1" class="w-[8px] h-[8px] rounded-full bg-gray-300" />
+    <div class="flex flex-row items-start justify-start w-full gap-4 ml-4">
+      <div class="flex-shrink-0 w-10 h-10 mt-1">
+        <img :src="achievement.image" :alt="achievement.title" class="w-full h-full object-cover rounded-lg" />
       </div>
-      <div class="flex flex-row items-center justify-center w-full h-full gap-2">
-        <div class="flex flex-col items-start justify-center w-full gap-1">
-          <div class="text-sm font-medium">{{achievement.title}}</div>
-          <div class="text-xs font-medium text-gray-400">
-            <p>{{achievement.year}}</p>
+      <div class="flex flex-col items-start justify-center w-full">
+        <div class="text-lg font-semibold">{{ achievement.title }}</div>
+        <div class="text-sm font-medium text-gray-400">
+          <p>{{ achievement.year }}</p>
+        </div>
+        <div class="flex items-start mt-2">
+          <div class="flex flex-col">
+            <div v-for="(winner, index) in achievement.winners" :key="index" class="text-base">
+              <div class="flex">
+                <div class="flex flex-col items-center justify-center w-5 h-full gap-3 py-2 mr-4">
+                  <div class="w-[8px] h-[8px] rounded-full bg-gray-300" />
+                  <div v-if="index !== achievement.winners.length - 1" class="h-full min-h-[30px] border-r border-gray-300" />
+                </div>
+                {{ winner.description }}
+              </div>
+            </div>
           </div>
         </div>
       </div>
